43676b36df10d9ca57babcdc634db9bb7d19762f
Patrick Drechsler <patrick@pdrechsler.de>
2019-02-21 23:06:25 +0100

    csharp: add NonEmptyString

---
+++ code/csharp/contact-list/ContactList.Tests/NonEmptyStringTests.cs
@@ -0,0 +1,35 @@
+using System;
+using CSharpFunctionalExtensions;
+using FluentAssertions;
+using Xunit;
+
+namespace ContactList.Tests
+{
+    public class NonEmptyStringTests
+    {
+        [Theory]
+        [InlineData("a", true)]
+        [InlineData("", false)]
+        [InlineData((string)null, false)]
+        public void NonEmptyString_creation_works(string input, bool isValid)
+        {
+            // Act
+            var result = NonEmptyString.Create(input);
+            
+            // Assert
+            result.Should()
+                .NotBeNull()
+                .And.BeOfType<Maybe<NonEmptyString>>();
+
+            if (isValid)
+            {
+                result.HasValue.Should().BeTrue();
+                result.Value.Value.Should().Be(input);
+            }
+            else
+            {
+                result.HasNoValue.Should().BeTrue();
+            }
+        }
+    }
+}
---
+++ code/csharp/contact-list/ContactList/NonEmptyString.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using CSharpFunctionalExtensions;
+
+namespace ContactList
+{
+    public class NonEmptyString : ValueObject
+    {
+        private NonEmptyString(string potentialString)
+        {
+            if (string.IsNullOrWhiteSpace(potentialString))
+            {
+                throw new ArgumentException();
+            }
+
+            Value = potentialString;
+        }
+
+        public string Value { get; }
+        
+        public static Maybe<NonEmptyString> Create(string potentialString)
+        {
+            Maybe<NonEmptyString> result;
+
+            try
+            {
+                result = Maybe<NonEmptyString>.From(new NonEmptyString(potentialString)); 
+            }
+            catch (Exception)
+            {
+                result = Maybe<NonEmptyString>.None;
+            }
+            return result;            
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Value;
+        }
+    }
+}
