3718763dcffb0df42cc5f49180bcb271ebab9de4
Patrick Drechsler <patrick@pdrechsler.de>
2019-02-28 22:50:49 +0100

    after preparation 1

---
+++ code/csharp/addressbook/Addressbook.Tests/Helper/Parser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LaYumba.Functional;
+
+namespace Addressbook.Tests.Helper
+{
+    public static class Parser
+    {
+        public static Option<DateTime> Parse(this string s)
+        {
+            return DateTime.TryParse(s, out var dt) 
+                ? F.Some(dt) 
+                : F.None;
+        }
+    }
+}
---
+++ code/csharp/addressbook/Addressbook.Tests/Helper/ParserTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentAssertions;
+using LaYumba.Functional;
+using Xunit;
+
+namespace Addressbook.Tests.Helper
+{
+    public class ParserTests
+    {
+        [Fact]
+        public void FactMethodName()
+        {
+            var option = "2019-01-01".Parse();
+            
+            option.Should().BeOfType<Option<DateTime>>();
+            
+            option.Match(
+                None: () => false.Should().BeTrue(),
+                Some: x => x.Should().Be(new DateTime(2019, 1, 1)));
+        }
+    }
+}
