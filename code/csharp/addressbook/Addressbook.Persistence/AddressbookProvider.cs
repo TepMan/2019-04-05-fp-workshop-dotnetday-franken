8decfc02f924a66970501ede130a510d2c8bf777
Patrick Drechsler <patrick@pdrechsler.de>
2019-03-06 23:31:33 +0100

    csharp: add some virtual folders & projects for persistence and ui - nothing works, just an idea..

---
+++ code/csharp/addressbook/Addressbook.Persistence/AddressbookProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Addressbook.Persistence.Contracts;
+
+namespace Addressbook.Persistence
+{
+    public class AddressbookProvider : IAddressbookProvider
+    {
+        public Addressbook GetAddressbook()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
