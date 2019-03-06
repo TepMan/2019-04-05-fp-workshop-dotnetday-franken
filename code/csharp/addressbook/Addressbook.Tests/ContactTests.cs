a19e8c47fd64e0ed77b478af87b256e258c5f2b3
Patrick Drechsler <patrick@pdrechsler.de>
2019-03-06 20:23:47 +0100

    csharp: change first and last name

--- code/csharp/addressbook/Addressbook.Tests/ContactTests.cs
+++ code/csharp/addressbook/Addressbook.Tests/ContactTests.cs
@@ -13,6 +13,44 @@ namespace Addressbook.Tests
         [Fact]
         public void Creating_a_new_contact_happy_path()
         {
+            var contact = CreateHomer();
+
+            contact.FirstName.Value.Should().Be("Homer");
+            contact.LastName.Value.Should().Be("Simpson");
+            contact.DateOfBirth.Should().BeEquivalentTo(Some(new DateTime(1956, 5, 12)));
+            contact.TwitterProfileUrl.Should()
+                .BeEquivalentTo(Some(NonEmptyString.CreateBang("https://twitter.com/homerjsimpson")));
+            contact.PrimaryContactMethod.Should().BeOfType<EmailContact>();
+            contact.OtherContactMethods.Should().NotBeNull().And.BeEmpty();
+        }
+
+        
+
+        [Theory]
+        [InlineData("Lisa", "Flanders", true, true)]
+        [InlineData("", "Flanders", false, true)]
+        [InlineData("Lisa", "", true, false)]
+        public void Changing_name_works_and_does_not_modify_id(
+            string newFirstName, string newLastName, 
+            bool isFirstNameValid, bool isLastNameValid)
+        {
+            // Arrange
+            var contact = CreateHomer();
+            
+            // Act
+            var result = contact
+                .ChangeFirstName(NonEmptyString.Create(newFirstName))
+                .ChangeLastName(NonEmptyString.Create(newLastName));
+            
+            // Assert
+            result.FirstName.Value.Should().Be(isFirstNameValid ? newFirstName : contact.FirstName.Value);
+            result.LastName.Value.Should().Be(isLastNameValid ? newLastName : contact.LastName.Value);
+            result.Id.Should().Be(contact.Id);
+        }
+        
+        
+        private static Contact CreateHomer()
+        {
             var firstname = NonEmptyString.CreateBang("Homer");
             var lastname = NonEmptyString.CreateBang("Simpson");
 
@@ -25,14 +63,7 @@ namespace Addressbook.Tests
 
             var contact = new Contact(id, firstname, lastname,
                 dob, twitterProfileUrl, new EmailContact(), new List<ContactMethod>());
-
-            contact.FirstName.Value.Should().Be("Homer");
-            contact.LastName.Value.Should().Be("Simpson");
-            contact.DateOfBirth.Should().BeEquivalentTo(Some(new DateTime(1956, 5, 12)));
-            contact.TwitterProfileUrl.Should()
-                .BeEquivalentTo(Some(NonEmptyString.CreateBang("https://twitter.com/homerjsimpson")));
-            contact.PrimaryContactMethod.Should().BeOfType<EmailContact>();
-            contact.OtherContactMethods.Should().NotBeNull().And.BeEmpty();
+            return contact;
         }
     }
 }
--- code/csharp/addressbook/Addressbook/Contact.cs
+++ code/csharp/addressbook/Addressbook/Contact.cs
@@ -8,6 +8,7 @@ using static LaYumba.Functional.F;
 namespace Addressbook
 {
     // Mmh, not sure yet if this class should also be a Value Object...
+    // Probably not, because this object has an identity, and therefore is an entity (not a value object)
     public class Contact
     {
         public Contact(Guid id,
@@ -28,13 +29,32 @@ namespace Addressbook
         }
 
         public Guid Id { get; }
-        public NonEmptyString FirstName { get; }
-        public NonEmptyString LastName { get; }
+        public NonEmptyString FirstName { get; private set; }
+        public NonEmptyString LastName { get; private set; }
         public Option<NonEmptyString> TwitterProfileUrl { get; }
         public Option<DateTime> DateOfBirth { get; }
         public ContactMethod PrimaryContactMethod { get; }
         public IEnumerable<ContactMethod> OtherContactMethods { get; }
 
+
+        public Contact ChangeFirstName(Option<NonEmptyString> optFirstName)
+        {
+            optFirstName.Match(
+                () => Unit(),
+                fn => FirstName = fn);
+            
+            return this;
+        }
+        
+        public Contact ChangeLastName(Option<NonEmptyString> optLastName)
+        {
+            optLastName.Match(
+                () => Unit(),
+                fn => LastName = fn);
+            
+            return this;
+        }
+        
         // TODO Add an actual address ;-)
     }
 }
