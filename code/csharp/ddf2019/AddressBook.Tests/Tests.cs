8039d14ac8b21abb85b40f0b4e7d70b79420ea18
Patrick Drechsler <patrick.drechsler@redheads.de>
2019-04-03 22:33:43 +0200

    csharp: add filter-map-reduce example

--- code/csharp/ddf2019/AddressBook.Tests/Tests.cs
+++ code/csharp/ddf2019/AddressBook.Tests/Tests.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
 using Xunit;
 
 namespace AddressBook.Tests
@@ -10,6 +12,34 @@ namespace AddressBook.Tests
         {
             true.Should().BeTrue();
             new Class1().SayHello("test").Should().Be("Hello, test!");
+
         }
+
+        [Fact]
+        public void FilterMapReduceDemo()
+        {
+            // Arrange
+            var people = new List<Person>
+            {
+                new Person { Age = 20, Income = 1000 },
+                new Person { Age = 26, Income = 1100 },
+                new Person { Age = 35, Income = 1300 }
+            };
+
+            // Act
+            var averageIncomeAbove25 = people
+                .Where(x => x.Age > 25)
+                .Select(x => x.Income)
+                .Average();
+            
+            // Assert
+            averageIncomeAbove25.Should().Be(1200);
+        }
+    }
+
+    public class Person
+    {
+        public int Age { get; set; }
+        public int Income { get; set; }
     }
 }
