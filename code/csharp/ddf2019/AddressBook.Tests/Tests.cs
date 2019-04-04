9273a88e28f2a8dc404e6745bffb2b4647017b0e
Patrick Drechsler <patrick.drechsler@redheads.de>
2019-04-04 02:21:34 +0200

    csharp: does not compile. but why?

--- code/csharp/ddf2019/AddressBook.Tests/Tests.cs
+++ code/csharp/ddf2019/AddressBook.Tests/Tests.cs
@@ -1,7 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
+using LaYumba.Functional;
 using Xunit;
+
+using static LaYumba.Functional.F;
 
 namespace AddressBook.Tests
 {
@@ -13,6 +16,33 @@ namespace AddressBook.Tests
             true.Should().BeTrue();
             new Class1().SayHello("test").Should().Be("Hello, test!");
 
+        }
+
+        [Fact]
+        public void FilterMapReduce_imperative_Demo()
+        {
+            // Arrange
+            var people = new List<Person>
+            {
+                new Person { Age = 20, Income = 1000 },
+                new Person { Age = 26, Income = 1100 },
+                new Person { Age = 35, Income = 1300 }
+            };
+
+            // Act
+            var incomes = new List<int>();
+            foreach (var person in people)
+            {
+                if (person.Age > 25)
+                {
+                    incomes.Add(person.Income);
+                }
+            }
+
+            var avg = incomes.Sum() / incomes.Count;
+
+            // Assert
+            avg.Should().Be(1200);
         }
 
         [Fact]
@@ -49,11 +79,40 @@ namespace AddressBook.Tests
             averageIncomeAbove25.Should().Be(1200);
             avg.Should().Be(1200);
         }
+
+        private class Person
+        {
+            public int Age { get; set; }
+            public int Income { get; set; }
+        }
+
+
+        [Fact]
+        public void Option_vs_Either_Demo()
+        {
+            Option<string> IsValidOpt(string s) =>
+                string.IsNullOrEmpty(s)
+                    ? None
+                    : Some(s);
+
+            // nasty cast
+            Either<string, string> IsValidEither(string s)
+                =>
+                    string.IsNullOrEmpty(s)
+                        ? (Either<string, string>) Left("ups")
+                        : Right(s);
+
+            Either<string, string> IsValidEither(Option<string> optS)
+                =>
+                    optS.Match(
+                        () => Left("ups"),
+                        x => Right(x));
+        }
+
+        class MyError
+        {
+            public string Message { get; set; }
+        }
     }
 
-    public class Person
-    {
-        public int Age { get; set; }
-        public int Income { get; set; }
-    }
 }
