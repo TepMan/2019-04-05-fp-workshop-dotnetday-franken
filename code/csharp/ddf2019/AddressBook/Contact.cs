a20369125acddf272bc7db3b2f4ae099fd434c67
Patrick Drechsler <patrick.drechsler@redheads.de>
2019-04-05 09:59:31 +0200

    Codeblock 2

--- code/csharp/ddf2019/AddressBook/Contact.cs
+++ code/csharp/ddf2019/AddressBook/Contact.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
 
 namespace AddressBook
 {
@@ -6,7 +8,34 @@ namespace AddressBook
     {
         public string FirstName { get;  }
         public string LastName { get;  }
-        public DateTime? DateOfBirth { get; }
-        public string TwitterHandler { get;  }
+        public Option<DateTime> DateOfBirth { get; }
+        public Option<string> TwitterHandler { get;  }
+
+        public Contact(string firstName, string lastName, DateTime? dateOfBirth, string twitterHandler)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            DateOfBirth = NullableDateTimeToOption(dateOfBirth);
+            TwitterHandler = StringToOption(twitterHandler);
+        }
+
+        private Option<DateTime> NullableDateTimeToOption(DateTime? date)
+        {
+            if (date == null)
+            {
+                return None;
+            }
+
+            return Some(date.Value);
+        }
+        private Option<string> StringToOption(string value)
+        {
+            if (value == null)
+            {
+                return None;
+            }
+
+            return Some(value);
+        }
     }
 }
