5684daecaf7197d1c3545c581b969421a2fc57b3
Patrick Drechsler <patrick.drechsler@redheads.de>
2019-04-05 12:59:32 +0200

    Codeblock 7 (Bind)

---
+++ code/csharp/ddf2019/AddressBook/Persistenz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using LaYumba.Functional;
+using static LaYumba.Functional.F;
+
+namespace AddressBook
+{
+    public static class Persistenz
+    {
+        public static Option<string> Save(this string content)
+        {
+            if (content.StartsWith("x"))
+                return Some(content);
+
+            return None;
+        }
+
+        public static Option<string> SendMessage(this string content)
+        {
+            Console.WriteLine("Irgendwas");
+            return Some(content);
+        }
+
+        public static Option<string> SaveAndSendMessage(this string content)
+        {
+            Option<string> result1 = Save(content);
+            return result1.Bind(SendMessage);
+        }
+
+    }
+}
